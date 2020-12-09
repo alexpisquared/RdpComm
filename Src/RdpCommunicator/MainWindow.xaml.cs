ebe9bdca75b98c958dc6d94dcd818483510e1ec9
AlexPi <alex.pigida@outlook.com>
2020-12-09 14:28:25 -0500

    Moved from TFC. Ported to .NET 5.0.

---
+++ Src/RdpCommunicator/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace RdpCommunicator
+{
+  public partial class MainWindow : Window
+  {
+    const string _ext = ".rdc", _core = "`", _appstg = @"appstg.json";
+    readonly string _rqst;
+    readonly string _resp;
+    readonly string _path = @"C:\temp\";
+    string _ansr = "·";
+    bool _allowExit = false;
+    int _patienceInSec = 20;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      pnlThanks.Visibility = Visibility.Hidden;
+      if (File.Exists(_appstg))
+        _path = File.ReadAllText(_appstg);
+
+      if (string.IsNullOrEmpty(_path) || !File.Exists(_appstg))
+      {
+        File.WriteAllText(_appstg, _path);
+      }
+
+      _rqst = _path + _core + _ext;
+      _resp = _path + "~" + _ext;
+    }
+
+    void Window_Loaded(object sender, RoutedEventArgs re)
+    {
+      if (File.Exists(_rqst))
+      {
+        lblRqst.Content = File.ReadAllText(_rqst);
+        new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Background, new EventHandler((s, e) => updateClock()), Dispatcher.CurrentDispatcher).Start();
+
+        Opacity = 1; Visibility = Visibility.Visible; // still flickers on XP => off screen offset is the solution:
+        //taken care by animation: Top = 10;
+      }
+      else
+      {
+        lblHey.Content = "No request file...";
+#if DevOps
+        new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Background, new EventHandler((s, e) => updateClock()), Dispatcher.CurrentDispatcher).Start();
+        lblRqst.Content = "Debug non-request-checking mode";
+        _allowExit = true;
+#else
+        App.Current.Shutdown();
+#endif
+      }
+      //Task.Factory.StartNew(() => Thread.Sleep(1000)).ContinueWith(_ => {        pnlMain.Background = Brushes.Brown;      }, TaskScheduler.FromCurrentSynchronizationContext());
+    }
+
+    void updateClock()
+    {
+      //btnDefault.Content = (--_patienceInSec) % 2 == 0 ? "Now" : $"In {_patienceInSec} sec";
+      btnDefault.Content = $"Now ({--_patienceInSec})";
+      if (_patienceInSec <= 0)
+      {
+        if (_ansr == "·")
+          thankAndCLose(" Nobody's there - Timed out ");
+      }
+    }
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+      base.OnClosing(e);
+
+      try
+      {
+        e.Cancel = !_allowExit;
+        lblRqst.Content = _allowExit ? "OK" : "Please press the button matching your ETA";
+
+        if (_allowExit && File.Exists(_rqst))
+        {
+          var newName = $"{_ansr} from {DateTime.Now:HH·mm}";
+          File.Move(_rqst, _rqst.Replace(_core, newName));
+#if DEBUG
+          Thread.Sleep(2500);
+          File.Move(_rqst.Replace(_core, newName), _rqst);
+#endif
+        }
+      }
+      catch (Exception ex) { Debug.WriteLine(ex); if (Debugger.IsAttached) Debugger.Break(); }
+    }
+    void onClose(object sender, RoutedEventArgs e)
+    {
+      _allowExit = true;
+      App.Current.Shutdown();
+    }
+
+    void onClick(object sender, RoutedEventArgs e) => thankAndCLose(((Button)sender).Content.ToString());
+
+    void thankAndCLose(string ansr)
+    {
+      try
+      {
+        _allowExit = true;
+        lblHey.Visibility =
+        lblRqst.Visibility =
+        lblBttm.Visibility =
+        pnlButtns.Visibility = Visibility.Hidden;
+        pnlThanks.Visibility = Visibility.Visible;
+        //File.WriteAllText(_resp, $"{DateTime.Now:MMMdd-HH:mm} + {_ansr}");
+
+        new DispatcherTimer(TimeSpan.FromSeconds(10), DispatcherPriority.Background, new EventHandler((s, e) => { App.Current.Shutdown(); }), Dispatcher.CurrentDispatcher).Start();
+
+        //Task.Factory.StartNew(() => Thread.Sleep(750)).ContinueWith(_ => { App.Current.Shutdown(); }, TaskScheduler.FromCurrentSynchronizationContext());
+
+        switch (ansr)
+        {
+          case "In 5 min":  /**/ _ansr += $" At {DateTime.Now.AddMinutes(05):HH·mm} ...or '{ansr}'"; break;
+          case "In 20 min": /**/ _ansr += $" At {DateTime.Now.AddMinutes(20):HH·mm} ...or '{ansr}'"; break;
+          case "In 1 hour": /**/ _ansr += $" At {DateTime.Now.AddMinutes(60):HH·mm} ...or '{ansr}'"; break;
+          default: _ansr += ansr; break;
+        }
+      }
+      catch (Exception ex) { Debug.WriteLine(ex); if (Debugger.IsAttached) Debugger.Break(); }
+    }
+
+    public static void ExecuteAfter(Action action, int milliseconds)
+    {
+      System.Threading.Timer timer = null;
+      timer = new System.Threading.Timer(s =>
+      {
+        action();
+        timer.Dispose();
+        lock (_timers)
+          _timers.Remove(timer);
+      }, null, milliseconds, uint.MaxValue - 10);
+      lock (_timers)
+        _timers.Add(timer);
+    }
+
+    static readonly HashSet<System.Threading.Timer> _timers = new HashSet<System.Threading.Timer>();
+  }
+}
---
+++ Src/RdpQuester/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace RdpQuester
+{
+  public partial class MainWindow : AAV.WPF.Base.WindowBase
+  {
+    const string _ext = ".rdc", _questPending = "`", _appstg = @"RdpQuester.appstg.json";
+    readonly string _rqstFile;
+    readonly string _path = @"C:\temp\";
+    string _ansr = "·";
+    int _patienceInSec = 20;
+    readonly TimeSpan _pollingPeriod = new TimeSpan(0, 5, 0);
+
+    static DateTimeOffset Eta(DateTimeOffset tn)
+    {
+      var cm = tn.Minute > 2 ? tn.Minute : tn.Minute + 60;
+      var nm = (cm) + (5 - ((cm - 2) % 5)) + (tn.Minute > 2 ? 0 : -60) + .33;
+      var eta = new DateTime(tn.Year, tn.Month, tn.Day, tn.Hour, 0, 0).AddMinutes(nm);
+      Debug.WriteLine($"{tn.Minute,3}   {cm,3}   { nm,3}     {tn}   {eta}");
+      return eta;
+    }
+
+    public MainWindow()
+    {
+      //var tn = new DateTimeOffset(2020, 7, 16, 23, 0, 22);      for (int i = 0; i < 5; i++)   Eta(tn.AddMinutes(i));      for (int i = 55; i < 60; i++) Eta(tn.AddMinutes(i));
+
+      InitializeComponent();
+      pnlThanks.Visibility = Visibility.Hidden;
+
+      if (File.Exists(_appstg))
+        _path = File.ReadAllText(_appstg);
+#if DEBUG
+      else
+        File.WriteAllText(_appstg, _path);
+#endif
+
+      _rqstFile = _path + _questPending + _ext;      //_resp = _path + "~" + _ext;
+    }
+
+    void Window_Loaded(object s, RoutedEventArgs re)
+    {
+      var now = DateTimeOffset.Now;
+      if (File.Exists(_rqstFile))
+      {
+        var fi = new FileInfo(_rqstFile);
+        if ((fi.CreationTime - now) < _pollingPeriod || (fi.LastWriteTime - now) < _pollingPeriod)
+        {
+          lblBttm.Content = $"BTW, a request already pending ... ";
+        }
+      }
+    }
+
+    void updateClock()
+    {
+      //btnDefault.Content = (--_patienceInSec) % 2 == 0 ? "Now" : $"In {_patienceInSec} sec";
+      btnDefault.Content = $"Now ({--_patienceInSec})";
+      if (_patienceInSec <= 0)
+      {
+        if (_ansr == "·")
+          sendRqst(" Nobody's there - Timed out ");
+      }
+    }
+
+    void onClose(object s, RoutedEventArgs e) => Application.Current.Shutdown();
+
+    void onClick(object s, RoutedEventArgs e) => sendRqst(((Button)s).Content.ToString());
+
+    void sendRqst(string quest)
+    {
+      try
+      {
+        switch (quest)
+        {
+          case "10 min":      /**/ _ansr += $" At {DateTimeOffset.Now.AddMinutes(05):HH·mm} ...or '{quest}'"; lblRqst.Content = $"I need access for {quest}"; break;
+          case "1 hour":      /**/ _ansr += $" At {DateTimeOffset.Now.AddMinutes(20):HH·mm} ...or '{quest}'"; lblRqst.Content = $"I need access for {quest}"; break;
+          case "Till EODay":  /**/ _ansr += $" At {DateTimeOffset.Now.AddMinutes(60):HH·mm} ...or '{quest}'"; lblRqst.Content = $"I need the box until end of day"; break;
+          case "Rebooting":   /**/ _ansr += $" At {DateTimeOffset.Now.AddMinutes(60):HH·mm} ...or '{quest}'"; lblRqst.Content = $"The box needs to be rebooted"; break;
+          default: _ansr += quest; break;
+        }
+
+        pnlButtns.Visibility = Visibility.Hidden;
+        lblBttm.Content = "The following request has been sent:";
+
+        foreach (var file in Directory.GetFiles(_path, $"*{_ext}")) File.Delete(file);
+        File.WriteAllText(_rqstFile, $"{lblRqst.Content}");
+
+        var now = DateTimeOffset.Now;
+        _nextSched = Eta(now);
+
+        _timer = new DispatcherTimer(TimeSpan.FromSeconds(.96), DispatcherPriority.Background, new EventHandler((s, e) => checkAndInform()), Dispatcher.CurrentDispatcher); //tu: one-line timer
+      }
+      catch (Exception ex) { Debug.WriteLine(ex); if (Debugger.IsAttached) Debugger.Break(); }
+    }
+
+
+    void checkAndInform()
+    {
+      var rdc = Directory.GetFiles(_path, $"*{_ext}").FirstOrDefault(f => Path.GetFileNameWithoutExtension(f) != _questPending);
+      if (rdc != null)
+      {
+        Opacity = 1;
+        _timer.Stop();
+        lblBttm.Content = "The following response jsut arrived:";
+        lblRqst.Content = $"{Path.GetFileNameWithoutExtension(rdc)}";
+        lblFdbk.Content = $"";
+        lblRqst.FontSize = 16;
+        File.Delete(rdc);
+      }
+      else
+      {
+        Opacity = .7;
+        _timeLeft = _nextSched - DateTimeOffset.Now;
+        lblFdbk.Content = $"Expect an answer in {_timeLeft:mm\\:ss} min, at{_nextSched:HH:mm}.";
+      }
+    }
+
+    TimeSpan _timeLeft;
+    DateTimeOffset _nextSched = DateTimeOffset.MinValue;
+    private DispatcherTimer _timer;
+  }
+}
